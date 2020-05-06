2373767af773040094d33382762d0892e5b0ac44
Hoa <danghuyhoa@gmail.com>
2020-05-06 11:02:25 +0700

    Demo HttpClient with HackerNews

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import * as news from './data/news.json';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hackernews';
+
+  articles: Article[] = news.default;
+  message: string;
+  isLoading = false;
+  loadingMessage = "Đang chờ...";
+
+  constructor(private httpClient: HttpClient) {
+    this.getNewsFromAPI();
+    console.log(news);
+  }
+
+  getNewsFromAPI() {
+    this.isLoading = true;
+    let interval = setInterval(() => {
+      this.loadingMessage += '.';
+    }, 500);
+    this.httpClient
+      .get<Article[]>('https://api.hnpwa.com/v0/news/1.json')
+      .subscribe( (newsItems) => {
+      this.articles = newsItems;
+        this.isLoading = false;
+        clearInterval(interval);
+    }, error => {
+        this.message = 'Gặp lỗi khi lấy danh sách bài viết từ HackerNewss';
+        this.isLoading = false;
+        clearInterval(interval);
+      });
+  }
+
+  createNews() {
+    const article: Article = {
+      title: 'Abc Xyz',
+      url: 'http://codegym.vn/blogs/huong-dan-angular'
+    }
+    this.httpClient.post('https://api.hnpwa.com/v0/news', article).subscribe( (result) => {
+      console.log('Thêm bài viết thành công');
+    }, (error) => {
+      console.log('Gặp lỗi khi thêm bài viết');
+      console.error(error);
+    });
+  }
+
+  showArticleContent(event) {
+    alert(event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
+import { DemoComponentComponent } from './demo-component/demo-component.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent,
+    DemoComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
